f870b9f6334826ee1d0c1d4062b8e467a56320e7
cmdspeed <102898515+cmdspeed@users.noreply.github.com>
2023-06-18 10:46:05 +0000

    Deploying to gh-pages from @ cmdspeed/goit-react-hw-05-movies@eaacc1eb143ad93c751d8275dd9631e011114c42 🚀

---
+++ static/js/536.932d41df.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[536],{536:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"test"})}}}]);
+//# sourceMappingURL=536.932d41df.chunk.js.map
